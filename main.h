9abb02c6e3f95d1b89b5387d2e767725e7c2bf5e
Ibrahim <mamansani.ibrahim01@gmail.com>
2023-11-14 22:53:50 -0800

    update main.h

--- main.h
+++ main.h
@@ -17,8 +17,77 @@
 #define READ_BUFFER_SIZE 4096
 #define WRITE_BUFFER_SIZE 2048
 #define BUFF_FLUSH -1
-	
+
+
 extern char **environ;
+
+
+/**
+* struct nodelist - silngle list
+* @num: the num to field
+* @str: a str
+* @next: points
+*/
+typedef struct nodelist
+{
+	int num;
+	char *str;
+	struct nodelist *next;
+} node_t;
+
+/**
+* struct infopass - contains args
+* @arg: a str
+* @argv: an arrray
+* @path: path
+* @argc: the arg count
+* @line_count: line count
+* @line_count_flag: line count flag
+* @fname: file name
+* @env: env
+* @err_num: eror number
+* @environ: custom env
+* @history: hist
+* @alias: alias
+* @env_changed: changd env
+* @status: status
+* @cmd_buff: add pointer
+* @cmd_buff_type: cmd type
+* @readfd: the fd
+* @histcount: count hist num
+*/
+typedef struct infopass
+{
+	char *arg;
+	char **argv;
+	char *path;
+	unsigned int line_count;
+	int err_num;
+	int line_count_flag;
+	char *fname;
+	node_t *env;
+	node_t *history;
+	node_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+
+	char **cmd_buff;
+	int cmd_buff_type;
+	int readfd;
+	int histcount;
+} alias_t
+
+/**
+* struct builtlegacy - contains builtin
+* @type: the legacy cmd
+* @func: the function
+*/
+typedef struct builtlegacy
+{
+	char *type;
+	int (*func)(alias_t *);
+} builtin_tab;
 
 
 /*Prototype */
